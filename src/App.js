8f0e98836dc6bc5ec0cd75338be7fdb621a4f014
Anurag Singh <108252041+silentcoder014@users.noreply.github.com>
2025-01-09 14:40:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and CLick to reload.
+          Edit <code>src/App.js</code> and Edit to reload.
         </p>
         
         <a
